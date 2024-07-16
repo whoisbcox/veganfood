eeb21a0c20ff5ec6189b031fa22a637bc5233490
Brandon <whoisbcox@gmail.com>
2024-07-15 22:26:04 -0500

    Building Stripe functionality

--- server.ts
+++ server.ts
@@ -5,6 +5,18 @@ import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
 
+const storeItems = new Map([
+  [1, {priceInCents: 1000, name: 'Vegan Pancakes'}],
+  [2, {priceInCents: 1500, name: 'Tofu Scramble'}],
+]);
+
+// Import Stripe library
+import Stripe from 'stripe';
+
+const stripe = new Stripe('sk_test_51Pba9tRo35rgW6dlVY3ZPQDBpSpDeCkOnfuQ62UvTtaGQNJUP8Fjmcn5sZUnwgVQi32gDrYcBuRkdmbDF3u0mGZF00HNC0Bpdg', {
+  apiVersion: '2024-06-20',
+});
+
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
@@ -17,6 +29,9 @@ export function app(): express.Express {
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
 
+  // Parse JSON bodies
+  server.use(express.json());
+
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
@@ -24,6 +39,37 @@ export function app(): express.Express {
     maxAge: '1y',
     index: 'index.html',
   }));
+
+  // Define the Stripe checkout endpoint
+  server.post('/create-checkout-session', async (req, res) => {
+    try {
+      const session = await stripe.checkout.sessions.create({
+        line_items: req.body.items.map((item: {
+          quantity: number; id: number; 
+}) => {
+          const storeItem = storeItems.get(item.id);
+          return {
+            price_data: {
+              currency: 'usd',
+              product_data: {
+                name: storeItem?.name
+              },
+              unit_amount: storeItem?.priceInCents
+            },
+            quantity: item.quantity,
+          }
+        }),
+        mode: 'payment',
+        success_url: `${req.protocol}://${req.get('host')}?success=true`,
+        cancel_url: `${req.protocol}://${req.get('host')}?canceled=true`,
+      });
+
+      res.json({ url: session.url });
+    } catch (error) {
+      const errorMessage = error instanceof Error ? error.message : 'Unknown error';
+      res.status(500).send(errorMessage);
+    }
+  });
 
   // All regular routes use the Angular engine
   server.get('**', (req, res, next) => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,6 +53,28 @@ export class AppComponent implements OnInit, OnDestroy {
     }
   }
 
+  submitOrder() {
+    fetch('create-checkout-session', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        items: [
+          { id: 1, quantity: 3 },
+          { id: 2, quantity: 1 }
+        ]
+      })
+    }).then(res => {
+      if (res.ok) return res.json();
+      return res.json().then(json => Promise.reject(json));
+    }).then(({ url }) => {
+      window.location = url;
+    }).catch(e => {
+      console.log(e.error)
+    });
+  }
+
   calculateTax(amount: number, taxPercent: number) {
     return taxPercent * amount;
   };
